8259560150ccb8ff2ebe56e00e2472f311e6d52b
JHAIR <jhair.ccenhua.27@unsch.edu.pe>
2026-07-22 06:08:57 -0500

    Ordenar rutas backend Render

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,50 +3,38 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
-const path = require("path");
-
-
-
 const authRoutes = require("./routes/authRoutes");
 const categoriaRoutes = require("./routes/categoriaRoutes");
 const productoRoutes = require("./routes/productoRoutes");
 const movimientoRoutes = require("./routes/movimientoRoutes");
 const clienteRoutes = require("./routes/clienteRoutes");
 const ventaRoutes = require("./routes/ventaRoutes");
-
-const dashboardRoutes =
-require("./routes/dashboardRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
-
 
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 
-// Ruta principal
+
+// Ruta de prueba
+app.get("/", (req,res)=>{
+    res.json({
+        mensaje:"API Sistema de Inventario funcionando 🚀"
+    });
+});
 
 
-// Rutas de autenticación
+// Rutas API
 app.use("/api/auth", authRoutes);
 app.use("/api/categorias", categoriaRoutes);
 app.use("/api/productos", productoRoutes);
 app.use("/api/movimientos", movimientoRoutes);
 app.use("/api/clientes", clienteRoutes);
 app.use("/api/ventas", ventaRoutes);
-
 app.use("/api/dashboard", dashboardRoutes);
-
-app.use("/api/dashboard", dashboardRoutes);
-
-
-// Servir frontend
-app.use((req,res)=>{
-    res.sendFile(
-        path.join(__dirname,"../public/index.html")
-    );
-});
 
 
 module.exports = app;
